e313c8abe5785f2f71383ed824aa0d6578f555b5
TJ Smith <tj@jarvispro.io>
2026-04-20 19:36:13 +0000

    Brew formula update for anodizer version 0.1.0

--- Formula/anodizer.rb
+++ Formula/anodizer.rb
@@ -9,28 +9,28 @@ class Anodizer < Formula
 
   version "0.1.0"
 
-  # platform: aarch64-pc-windows-msvc
-  url "https://github.com/tj-smith47/anodizer/releases/download/v0.1.0/anodizer-0.1.0-windows-arm64.zip"
-
-  sha256 "cdf82fb3e5bbc5717285a4e151fa52f9129f638343b9e8c248f327b0c6fab5be"
   # platform: x86_64-pc-windows-msvc
   url "https://github.com/tj-smith47/anodizer/releases/download/v0.1.0/anodizer-0.1.0-windows-amd64.zip"
 
   sha256 "a263ab37fece034eafd741ae02cc5384a874a6fe452fa542bb43941717be4aa5"
-  on_macos do
-    on_arm do
-      url "https://github.com/tj-smith47/anodizer/releases/download/v0.1.0/anodizer-0.1.0-darwin-arm64.tar.gz"
+  # platform: aarch64-pc-windows-msvc
+  url "https://github.com/tj-smith47/anodizer/releases/download/v0.1.0/anodizer-0.1.0-windows-arm64.zip"
 
-      sha256 "02124a3072119f3363e64960cac47f3e1c0c380ef29075b3e908d4637064adfa"
+  sha256 "cdf82fb3e5bbc5717285a4e151fa52f9129f638343b9e8c248f327b0c6fab5be"
+  on_macos do
+    on_intel do
+      url "https://github.com/tj-smith47/anodizer/releases/download/v0.1.0/anodizer-0.1.0-darwin-amd64.tar.gz"
+
+      sha256 "67735eaa2665ad2985dc00c296e70da4fda946054301934013bb983e8742f0ef"
 
       def install
         bin.install "anodizer"
       end
     end
-    on_intel do
-      url "https://github.com/tj-smith47/anodizer/releases/download/v0.1.0/anodizer-0.1.0-darwin-amd64.tar.gz"
+    on_arm do
+      url "https://github.com/tj-smith47/anodizer/releases/download/v0.1.0/anodizer-0.1.0-darwin-arm64.tar.gz"
 
-      sha256 "67735eaa2665ad2985dc00c296e70da4fda946054301934013bb983e8742f0ef"
+      sha256 "02124a3072119f3363e64960cac47f3e1c0c380ef29075b3e908d4637064adfa"
 
       def install
         bin.install "anodizer"
